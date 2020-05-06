10b7664f28993d658004c0466470e64ee28b544f
Eddy Pastika <eddy.pastika@tanihub.com>
2020-05-07 01:04:27 +0700

    [ADD] http request with echo

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	arithmetic "github.com/eddypastika/go-arithmetic"
+	"github.com/labstack/echo"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -17,4 +21,36 @@ func main() {
 	s := arithmetic.Subtract(a, b)
 	fmt.Println("Subtract result: ", s)
 
+	e := echo.New()
+	e.GET("/calculate", arithmeticHandler)
+	e.Start(":9090")
+
+}
+
+func arithmeticHandler(c echo.Context) error {
+	var res int64
+
+	values := c.QueryParam("values")
+	op := c.QueryParam("operator")
+
+	arrValues := strings.Split(values, ",")
+	arrValuesInt := []interface{}{}
+	for _, v := range arrValues {
+		param, err := strconv.Atoi(v)
+		if err != nil {
+			return err
+		}
+		arrValuesInt = append(arrValuesInt, param)
+	}
+
+	switch strings.ToLower(op) {
+	case "multiply":
+		res = arithmetic.Multiply(arrValuesInt...)
+	case "add":
+		res = arithmetic.Add(arrValuesInt...)
+	case "subtract":
+		res = arithmetic.Subtract(arrValuesInt...)
+	}
+
+	return c.JSON(http.StatusOK, res)
 }
